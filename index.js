82d00b8d48b29031fd413af320ad2286fe3f4be8
Ronald DeBeras <rdeberas@gmail.com>
2018-03-13 13:12:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function currentLine(NewLine) {
     if (NewLine.length === 0) {
       return "The line is currently empty."
     } else {
-      for(var i = 0; i < x.length; i++) {
-        line += (i + 1) + ". " + x[i] + ", "
+      for(var i = 0; i < NewLine.length; i++) {
+        line += (i + 1) + ". " + NewLine[i] + ", "
       }
       line = line.slice(0, line.length-2)
       return "The line is currently: " + line
