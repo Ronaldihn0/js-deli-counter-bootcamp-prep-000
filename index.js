00e4eea0f5856386c6cbbe2d0e27a1fdb35294c3
Ronald DeBeras <rdeberas@gmail.com>
2018-03-13 13:46:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,16 +5,15 @@ function takeANumber(katzDeliLine, name) {
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
 
-function nowServing(x) {
-  if (x.length === 0) {
+function nowServing(Ada) {
+  if (Ada.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
     var name = x[0];
-    x.splice(0, 1);
+    Ada.splice(0, 1);
     return "Currently serving " + name + ".";
   }
 }
-
 
 function currentLine(NewLine) {
     var line = []
