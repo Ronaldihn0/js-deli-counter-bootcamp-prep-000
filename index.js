63af87cf00b3988a499f1d5daea7cd6411317529
Ronald DeBeras <rdeberas@gmail.com>
2018-03-13 14:04:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var katzDeliLine = [];
 
-function takeANumber(katzDeliLine, name) {
+function TakeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
 
-function nowServing(Ada) {
+function NowServing(Ada) {
   if (Ada.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
@@ -15,12 +15,13 @@ function nowServing(Ada) {
   }
 }
 
-function currentLine(NewLine) {
-    var line = []
+function CurrentLine(NewLine) {
+    var line = [];
     if (NewLine.length === 0) {
       return "The line is currently empty."
-    } else {
-      for(var i = 0; i < NewLine.length; i++) {
+    }
+     else {
+      for (var i = 0; i < NewLine.length; i++) {
         line += (i + 1) + ". " + NewLine[i] + ", "
       }
       line = line.slice(0, line.length-2)
