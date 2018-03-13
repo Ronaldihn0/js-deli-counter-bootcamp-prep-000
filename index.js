8296757832f448f705d1657099c18b01cd934b6f
Ronald DeBeras <rdeberas@gmail.com>
2018-03-13 13:20:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ function takeANumber(katzDeliLine, name) {
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line."
 }
 
+function nowServing(x) {
+  if (x.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var name = x[0];
+    x.splice(0, 1);
+    return "Currently serving " + name + ".";
+  }
+}
+
+
 function currentLine(NewLine) {
     var line = []
     if (NewLine.length === 0) {
@@ -17,13 +28,3 @@ function currentLine(NewLine) {
       return "The line is currently: " + line
     }
     //
-}
-function nowServing(x) {
-  if (x.length === 0) {
-    return "There is nobody waiting to be served!"
-  } else {
-    var name = x[0];
-    x.splice(0, 1);
-    return "Currently serving " + name + ".";
-  }
-}
